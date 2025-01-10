02cff51e6b96c50272a008de8192a00c7c5bc976
ENAA <khadijauser779@gmail.com>
2025-01-10 17:47:39 +0100

    first commit

---
+++ scrip.js
@@ -0,0 +1,11 @@
+ 
+
+$(document).ready(() => {
+    var typed = new Typed(".text", {
+        strings: ["frontend developer", "backend developer", "developer"], 
+        typeSpeed: 300, 
+        backSpeed: 300, 
+        backDelay: 300,
+        loop: true 
+    });
+});
