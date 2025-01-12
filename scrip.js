38cdd98a054f88cfa80b76bf1b7f7f4b5f38ca8e
khadijab <khadijauser779@gmail.com>
2025-01-12 23:34:19 +0100

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -23,45 +23,32 @@ $(document).ready(function() {
         $('#contactForm').submit(function(e) {
             e.preventDefault();
             
-            const name = $('#name').val().trim();
             const email = $('#email').val().trim();
-            const message = $('#message').val().trim();
             
             $('.error-message').remove();
             $('.form-control').removeClass('is-invalid');
             
             let isValid = true;
             
-            if (name.length < 2) {
-                $('#name').addClass('is-invalid');
-                $('#name').after('<div class="error-message">Nom requis (min 2 caractères)</div>');
-                isValid = false;
-            }
-            
             if (!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
-                $('#email').addClass('is-invalid');
-                $('#email').after('<div class="error-message">Email invalide</div>');
-                isValid = false;
-            }
-            
-            if (message.length < 10) {
-                $('#message').addClass('is-invalid');
-                $('#message').after('<div class="error-message">Message requis (min 10 caractères)</div>');
-                isValid = false;
+            $('#email').addClass('is-invalid');
+            $('#email').after('<div class="error-message">Email invalide</div>');
+            isValid = false;
             }
             
             if (isValid) {
-                $(this).after('<div class="alert alert-success">Message envoyé!</div>');
-                
-                this.reset();
-                
-                setTimeout(() => {
-                    $('.alert-success').fadeOut('slow', function() {
-                        $(this).remove();
-                    });
-                }, 3000);
+            $(this).after('<div class="alert alert-success">Email envoyé!</div>');
+            
+            this.reset();
+            
+            setTimeout(() => {
+                $('.alert-success').fadeOut('slow', function() {
+                $(this).remove();
+                });
+            }, 3000);
             }
         });
+
 
     $(window).on('scroll', function() {
         let scrollPosition = $(this).scrollTop();
